ab8b3286ecf2c5f71c88ef89056a5a56022f8341
Harish K V <kumarvinojari@gmail.com>
2026-02-12 09:18:12 +0530

    Update team.js

--- team.js
+++ team.js
@@ -31,10 +31,13 @@ If you’re looking for something real — welcome.`,
   { name: "Visva Sugin N R", role: "Head of UI/UX Design", desc: "Design-focused creative contributor. I'm Visvasugin, currently pursuing B.Tech CSBS at K. Ramakrishnan College of Engineering, after completing my HSLC at Karur Vetri Vinayaga Hr Sec School. I'm passionate about designing and editing, and I'm on a mission to overcome procrastination and build emotional intelligence. One of my strengths is adapting easily to people and environments, which I believe also sometimes becomes a weakness when I take on too much. Overall, I'm working on turning my passions into strengths and growing as a person", linkedin: "#" },
   { name: "Devi Priya P", role: "Social Media & Growth Manager", desc: "Growth-focused digital strategist.", linkedin: "https://www.linkedin.com/in/devipriya-p-7a8353327" },
   { name: "Gnyanodhaya S", role: "Content Research Analyst", desc: "Structured analytical thinker. I bring a strong blend of technology, data analytics, and creative skills to the team, making me well-suited for fast-paced startup environments. I take ownership of tasks and effectively translate ideas into scalable, real-world solutions. With hands-on experience in software development, cloud platforms, data analysis, and video editing, I support both product development and digital communication needs. I approach problems with a practical, growth-oriented mindset and adapt quickly to change. I consistently add value by combining analytical thinking with creativity to drive business and user impact.", linkedin: "https://www.linkedin.com/in/gnyanodhaya-s-4033b12a4?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
-  { name: "Arjun J S", role: "Head of Operations", desc: "Accurate and reliable executor.", linkedin: "#" },
-  { name: "Hariprakash S", role: "Technical Lead", desc: "Driven to grow and build.", linkedin: "#" },
-  { name: "Praveen P", role: "Head of Media Production", desc: "Calm, observant, detail-focused.", linkedin: "#" },
-  { name: "Gowtham S", role: "Head of Innovation & AI Systems", desc: "Passionate about meaningful software.", linkedin: "#" }
+  { name: "Arjun J S", role: "Head of Operations", desc: "Accurate and reliable executor. I am a dedicated and goal-oriented young professional who believes in continuous learning and self-improvement. I have strong analytical skills with a keen interest in accounting, aptitude, and cloud technologies. I manage my time effectively and always strive to improve my communication and professional skills. I am passionate about contributing to meaningful work, especially initiatives that help others and support green actions. I am self-aware, confident, and committed to growing into a responsible and impactful professional.", linkedin: "https://www.linkedin.com/in/arjun-js-a792b42a3?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
+  { name: "Hariprakash R", role: "Technical Lead", desc: "Driven to grow and build. I am Hariprakash.I’m passionate about technology and always eager to learn new things in the field of software development and design.", linkedin: "https://www.linkedin.com/in/hariprakash-r-4672b42a3?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
+  { name: "Praveen P", role: "Head of Media Production", desc: "Calm, observant, detail-focused.", linkedin: "https://www.linkedin.com/in/praveen-parankunran-3194302a3?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
+ // { name: "Thomas Livingston", role: "Head of Innovation & AI Systems", desc: "Passionate about meaningful software.", linkedin: "#" }
+ // { name: "Gowtham R", role: "Head of Innovation & AI Systems", desc: "Passionate about meaningful software.", linkedin: "#" }
+   
+   
 ];
 
 /* =========================
@@ -203,3 +206,4 @@ if (canvas) {
 }
 
 });
+
